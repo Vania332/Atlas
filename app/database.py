4e9ca42920d838271674e0fc11b48ed9b499bf75
Vania332 <drozhzha2007@gmail.com>
2025-12-12 03:04:04 +0300

    timely fixes

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from config import Settings
 settings = Settings()
 
 engine = create_engine(
-    url=settings.DATABASE_URL_mysql,
+    url=settings.DATABASE_URL_mysql(),
     echo=True,
     pool_size=5,
     max_overflow=10,
